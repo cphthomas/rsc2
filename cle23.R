79e8a8e66608fba9bda9b189177d38528f6248f5
cphthomas <cphthomas@gmail.com>
2019-04-14 22:26:49 +0000

    git

--- cle23.R
+++ cle23.R
@@ -2,3 +2,11 @@ cle <- 23
 cle
 
 test <- 232323
+
+# git config credential.helper cache
+git config --global credential.helper 'cache --timeout 3600'
+git push https://github.com/cphthomas/rsc2.git
+
+  
+  [work for 5 more minutes]
+git push https://github.com/cphthomas/rsc2.git
